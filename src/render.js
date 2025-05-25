440674e25966ab01b184e78ff00b897bf6ef93df
jessiiishin <jessiiishin@gmail.com>
2025-05-24 22:52:55 -0400

    video.js addede

---
+++ src/render.js
@@ -0,0 +1,6 @@
+// buttons
+// const musicBtn = document.getElementById("musicBtn");
+const timerBtn = document.getElementById("timerBtn");
+const notesBtn = document.getElementById("notesBtn");
+const settingsBtn = document.getElementById("settingsBtn");
+
