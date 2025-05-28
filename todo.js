5401f965157177b0ff213c35980c6d6f7e967da7
jessiiishin <jessiiishin@gmail.com>
2025-05-28 14:30:02 -0400

    todo list functionalities mostly complete

--- todo.js
+++ todo.js
@@ -13,11 +13,16 @@ input.addEventListener('keydown', (event) => {
 
 function addNewTask(taskText) {
     const newTask = document.createElement('li');
-    newTask.textContent = taskText;
+    const textSpan = document.createElement('span');
+    textSpan.textContent = taskText;
+    newTask.appendChild(textSpan);
+
+    newTask.addEventListener('dblclick', () => editTask(newTask, textSpan));
     
-    const checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.addEventListener('click', () => checkbox.check());
+    const checkbox = createCheckBox(newTask);
+    const delBtn = createDelBtn();
+    
+    newTask.appendChild(delBtn);
     newTask.prepend(checkbox);
 
     taskList.appendChild(newTask);
@@ -26,4 +31,60 @@ function addNewTask(taskText) {
 function markAsComplete(doneTask) {
     taskList.removeChild(doneTask);
     doneList.appendChild(doneTask);
+}
+
+function markAsNotComplete(undoneTask) {
+    doneList.removeChild(undoneTask);
+    taskList.appendChild(undoneTask);
+}
+
+function replaceInputWithText(textSpan, newTask, repInput) {
+    textSpan.textContent = repInput.value;
+    newTask.removeChild(repInput);
+    newTask.insertBefore(textSpan, newTask.firstChild.nextSibling);
+}
+
+function createCheckBox(task) {
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            markAsComplete(task);
+        } else {
+            markAsNotComplete(task);
+        }
+    });
+
+    return checkbox;
+}
+
+function createDelBtn() {
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'x';
+    delBtn.addEventListener('click', (event) => {
+        event.target.parentElement.remove();
+    });
+
+    return delBtn;
+}
+
+function editTask(task, textSpan) {
+    const repInput = document.createElement('input');
+    repInput.value = textSpan.textContent;
+
+    task.removeChild(textSpan);
+    task.insertBefore(repInput, task.firstChild.nextSibling);
+
+    repInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            replaceInputWithText(textSpan, task, repInput);
+        }
+    });
+
+    repInput.addEventListener('blur', () => {
+        replaceInputWithText(textSpan, task, repInput);
+    });
+
+    repInput.focus();
 }
