34efcfd02f3384e389ca05ebdcd90aa45eec0802
jessiiishin <jessiiishin@gmail.com>
2025-05-28 00:23:45 -0400

    checkbox and task list kind of mplemeented

--- todo.js
+++ todo.js
@@ -1,6 +1,29 @@
 
-document.getElementById('')
+const taskList = document.getElementById('taskList');
+const doneList = document.getElementById('doneList');
+const input = document.getElementById('taskInput');
+let taskArr = [];
 
-function addToTodoList(task) {
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addNewTask(input.value);
+        input.value = '';
+    }
+});
 
+function addNewTask(taskText) {
+    const newTask = document.createElement('li');
+    newTask.textContent = taskText;
+    
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.addEventListener('click', () => checkbox.check());
+    newTask.prepend(checkbox);
+
+    taskList.appendChild(newTask);
+}
+
+function markAsComplete(doneTask) {
+    taskList.removeChild(doneTask);
+    doneList.appendChild(doneTask);
 }
